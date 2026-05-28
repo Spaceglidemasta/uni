7aa370e7ddecb57edd5ff30d7f7fc88464a2bd47
Spaceglidemasta <lucastarwars1@gmail.com>
2026-05-28 13:47:30 +0200

    itsec ex3 blockchain it1

---
+++ semester4/it_sec/exercise3_blockchain.md
@@ -0,0 +1,11 @@
+# Exercise 3 (Blockchain)
+## 1
+
+1. Bad scenario for a blockchain. A blockchain is immutable, but not really secure towards the contained data and is only really used in decentralized scenarios. It also lacks efficiency.
+
+2. Good scenario for a blockchain. Decentralized situation with immutable, chainable data.
+
+3. Bad idea. Holiday requests may change / are not very keen to beeing immutable.
+
+## 2
+
--- semester4/rts/linux_exercises/interruptTimer.c
+++ semester4/rts/linux_exercises/interruptTimer.c
@@ -19,14 +19,15 @@ int main(void)
       
   // *********** Interrupt Service Routine to be called after timer interrupt ****************************
   // * get the time point of start of ISR and print it out, increase tick counter
-  void interrupt_service_routine () 
-  {
+  void interrupt_service_routine (int i) {
 
     struct timespec t_Start_isr  ;
 
     clock_gettime( CLOCK_REALTIME,  &t_Start_isr );
     printf("\n>%10ld %9ld ns -   -     Tick%i", t_Start_isr.tv_sec - tP_0.tv_sec, t_Start_isr.tv_nsec, cnt_tick);
     cnt_tick++;
+
+    
   }
   // *********** end of Interrupt Service Routine *******************************************************
 
@@ -40,17 +41,14 @@ int main(void)
   itval.it_value.tv_usec    = DT_WAIT_US;       // micro seconds to wait after first start of timer
   
   // define the alarm by specifying the alarm type and the ISR
-  signal (SIGALRM, interrupt_service_routine);
+  //signal (SIGALRM, (__sighandler_t) interrupt_service_routine);
   
-  // comment out the line above with signal and uncomment the following lines for sigaction
-  /*
   struct sigaction mysigaction;                         // initiate my sigaction as my sigactionset
   sigemptyset(&mysigaction.sa_mask);                    // clear mysigactionset
   mysigaction.sa_handler = interrupt_service_routine;   // fill my ISF into mysigactionset
   mysigaction.sa_flags = SA_RESTART;                    // set the SA_RESTART-flag in mysiagaction
 
   sigaction (SIGALRM, &mysigaction, NULL);              // activate my sigaction set
-  //*/
   
   // init and start the timer
   ret = setitimer (ITIMER_REAL, &itval, NULL);
