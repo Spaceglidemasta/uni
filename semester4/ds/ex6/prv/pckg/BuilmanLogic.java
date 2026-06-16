1ecbfe8e9e9fd684f3fb4a774bc50c7fa56ea447
Spaceglidemasta <lucastarwars1@gmail.com>
2026-06-16 21:38:16 +0200

    distsys ex6 06.16.26

--- semester4/ds/ex6/prv/pckg/BuilmanLogic.java
+++ semester4/ds/ex6/prv/pckg/BuilmanLogic.java
@@ -1,8 +1,23 @@
 package pckg;
 
+import java.util.ArrayList;
+
 
 
 public final class BuilmanLogic {
 
+    
+
+    static final public Building searchBuilding(ArrayList<Building> arr, String name){
+
+        Building out = null;
+
+        for (Building b : arr){
+            if(b.getName().equals(name)) out = b;
+        }
+
+        return out;
+
+    }
 
 }
