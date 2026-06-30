1e7d96b2bb570e0c6daf7a383683bf32b15636af
Spaceglidemasta <lucastarwars1@gmail.com>
2026-06-30 16:22:43 +0200

    rts extimes2 final

--- semester4/rts/linux_exercises/execution_times_2/run.sh
+++ semester4/rts/linux_exercises/execution_times_2/run.sh
@@ -7,7 +7,7 @@ fi
 
 gcc load.c -o load_1
 
-#copy program 5 times
+
 for i in {2..5}
 do
     cp load_1 load_$i
@@ -15,5 +15,6 @@ done
 
 for i in {1..5}
 do 
-    ./load_$i $i 20 15 $1 &
+    prio=19-$i
+    ./load_$i $i 20 $prio $1 &
 done
